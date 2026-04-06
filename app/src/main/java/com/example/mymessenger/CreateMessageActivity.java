8cbfb0fc2a2fa32f365bdaf4a9caef6038dd20d4
kohan81 <kohan81@gmail.com>
2026-04-06 23:13:52 +0100

    bug fixed and increased the size of the text field and changed the position of the word messenger in the text field

--- app/src/main/java/com/example/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/example/mymessenger/CreateMessageActivity.java
@@ -1,14 +1,7 @@
 package com.example.mymessenger;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import android.app.Activity;
-import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
@@ -22,7 +15,7 @@ public class CreateMessageActivity extends AppCompatActivity {
     }
     //Call onSendMessage() when the button is clicked
     public void onSendMessage(View view) {
-        EditText messageView = (EditText)findViewById(R.id.message);
+        EditText messageView = findViewById(R.id.message);
         String messageText = messageView.getText().toString();
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
